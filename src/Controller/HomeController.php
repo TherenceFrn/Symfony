4ae50f2ec1c7db75d25919a336d265df797ce32e
Therence Ferron <therence.ferron@gmail.com>
2024-09-24 19:43:52 +0200

    feat: add user on homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,12 +10,26 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\User;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
+        // Crée un nouvel utilisateur
+        $user = new User();
+        $user->setUsername('therence');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        // Hash le mot de passe
+        $hashedPassword = $passwordHasher->hashPassword($user, 'therence');
+        $user->setPassword($hashedPassword);
+
+        // Persiste l'utilisateur en base de données
+        $em->persist($user);
+        $em->flush();
+
         return $this->render('home/index.html.twig');
     }
 }
